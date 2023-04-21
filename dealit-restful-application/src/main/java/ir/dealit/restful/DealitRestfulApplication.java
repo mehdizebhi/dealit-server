ce383d528ea21c0f81b252011c99d1c658f9b250
mehdizebhi <mehdizebhi@yahoo.com>
2023-04-21 16:03:41 +0330

    Initialize Spring Boot 3.0.6 project for RESTful Application 'dealit-restful-application'

---
+++ dealit-restful-application/src/main/java/ir/dealit/restful/DealitRestfulApplication.java
@@ -0,0 +1,13 @@
+package ir.dealit.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealitRestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealitRestfulApplication.class, args);
+    }
+
+}
---
+++ dealit-restful-application/src/test/java/ir/dealit/restful/DealitRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.dealit.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealitRestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
