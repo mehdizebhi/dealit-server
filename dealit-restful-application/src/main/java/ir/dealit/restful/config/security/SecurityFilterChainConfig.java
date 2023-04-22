52e208e222a1478f76a47c99d7caa4025d3a2c95
mehdizebhi <mehdizebhi@yahoo.com>
2023-04-22 19:56:28 +0330

    Add basic logic for SecurityFilterChain class

--- dealit-restful-application/src/main/java/ir/dealit/restful/config/security/SecurityFilterChainConfig.java
+++ dealit-restful-application/src/main/java/ir/dealit/restful/config/security/SecurityFilterChainConfig.java
@@ -12,7 +12,11 @@ public class SecurityFilterChainConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic();
         return http.build();
     }
 }
