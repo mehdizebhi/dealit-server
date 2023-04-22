ea7a34d0b09a86c72f965ee553fad33cb208482a
mehdizebhi <mehdizebhi@yahoo.com>
2023-04-22 19:17:58 +0330

    Create User Model class

---
+++ dealit-restful-application/src/main/java/ir/dealit/restful/model/user/User.java
@@ -0,0 +1,32 @@
+package ir.dealit.restful.model.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Document
+@Data
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    private @MongoId ObjectId id;
+    private String username;
+    private String password;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+}
