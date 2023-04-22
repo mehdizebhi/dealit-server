3976baab3c2d8a5695f61a1ab37546f576dc72a6
mehdizebhi <mehdizebhi@yahoo.com>
2023-04-22 19:26:05 +0330

    update AuthenticationController

--- dealit-restful-application/src/main/java/ir/dealit/restful/controller/auth/AuthenticationController.java
+++ dealit-restful-application/src/main/java/ir/dealit/restful/controller/auth/AuthenticationController.java
@@ -1,4 +1,12 @@
 package ir.dealit.restful.controller.auth;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
+
 }
