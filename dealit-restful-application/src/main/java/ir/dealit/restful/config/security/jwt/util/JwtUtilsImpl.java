43094f0115c1330b49ae3dbef7fb575eb1a92743
mehdizebhi <mehdizebhi@yahoo.com>
2023-04-23 20:00:11 +0330

    Implement all methods that define in JwtUtils interface (JwtUtilsImpl)

--- dealit-restful-application/src/main/java/ir/dealit/restful/config/security/jwt/util/JwtUtilsImpl.java
+++ dealit-restful-application/src/main/java/ir/dealit/restful/config/security/jwt/util/JwtUtilsImpl.java
@@ -1,4 +1,83 @@
 package ir.dealit.restful.config.security.jwt.util;
 
-public class JwtUtilsImpl extends JwtUtils{
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtUtilsImpl implements JwtUtils {
+
+    @Value("${jwt.key}")
+    private static String SECRET_KEY;
+    private static long EXPIRATION_PERIOD = 86_400_000;
+
+    @Override
+    public String extractSubject(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    @Override
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    @Override
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    @Override
+    public Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyByte = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyByte);
+    }
+
+    @Override
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    @Override
+    public String generateToken(UserDetails user) {
+        return generateToken(new HashMap<>(), user);
+    }
+
+    @Override
+    public String generateToken(Map<String, Object> extraClaims, UserDetails user) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_PERIOD))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    @Override
+    public boolean isTokenValid(String token, UserDetails user) {
+        final String username = extractSubject(token);
+        return username.equals(user.getUsername()) && !isTokenExpired(token);
+    }
 }
