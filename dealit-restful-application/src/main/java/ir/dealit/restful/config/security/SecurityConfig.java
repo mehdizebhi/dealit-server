c912e369e33809641a637989be8d94c3f49e6a0c
mehdizebhi <mehdizebhi@yahoo.com>
2023-04-24 13:03:16 +0330

    add some Bean for security reason (AuthenticationProvider, PasswordEncoder, AuthenticationManager)

--- dealit-restful-application/src/main/java/ir/dealit/restful/config/security/SecurityConfig.java
+++ dealit-restful-application/src/main/java/ir/dealit/restful/config/security/SecurityConfig.java
@@ -2,15 +2,45 @@ package ir.dealit.restful.config.security;
 
 import ir.dealit.restful.config.security.jwt.util.JwtUtils;
 import ir.dealit.restful.config.security.jwt.util.JwtUtilsImpl;
+import ir.dealit.restful.service.user.UserDaoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDaoService userDaoService;
+
     @Bean
     @Primary
     public JwtUtils getJwtUtilsBean() {
         return new JwtUtilsImpl();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDaoService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
