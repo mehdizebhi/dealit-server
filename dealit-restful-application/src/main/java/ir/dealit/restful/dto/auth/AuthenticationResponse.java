bec246b097f0f90a658fe62428efdcef83403331
mehdizebhi <mehdizebhi@yahoo.com>
2023-04-24 13:25:51 +0330

    create dto class for login response

--- dealit-restful-application/src/main/java/ir/dealit/restful/dto/auth/AuthenticationResponse.java
+++ dealit-restful-application/src/main/java/ir/dealit/restful/dto/auth/AuthenticationResponse.java
@@ -1,2 +1,14 @@
-package ir.dealit.restful.dto.auth;public class AuthenticationResponse {
+package ir.dealit.restful.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
 }
