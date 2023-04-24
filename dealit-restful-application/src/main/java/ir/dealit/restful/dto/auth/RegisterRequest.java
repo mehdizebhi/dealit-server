ffd5448fbd0287bb9a0323840d1b9d246d591168
mehdizebhi <mehdizebhi@yahoo.com>
2023-04-24 13:26:18 +0330

    create dto class for sign-up requests

--- dealit-restful-application/src/main/java/ir/dealit/restful/dto/auth/RegisterRequest.java
+++ dealit-restful-application/src/main/java/ir/dealit/restful/dto/auth/RegisterRequest.java
@@ -1,2 +1,16 @@
-package ir.dealit.restful.dto.auth;public class RegisterRequest {
+package ir.dealit.restful.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String username;
+    private String password;
 }
