32b2c74bd3d048e776d71c470b9eeea56bcb2195
mehdizebhi <mehdizebhi@yahoo.com>
2023-04-24 11:42:59 +0330

    create BeansConfig class for create general bean for application

---
+++ dealit-restful-application/src/main/java/ir/dealit/restful/config/application/BeansConfig.java
@@ -0,0 +1,10 @@
+package ir.dealit.restful.config.application;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class BeansConfig {
+
+}
