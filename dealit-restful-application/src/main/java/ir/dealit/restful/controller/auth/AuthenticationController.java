91018493269890ba8829ea91aabed0a5ee78903a
mehdizebhi <mehdizebhi@yahoo.com>
2023-04-24 13:27:55 +0330

    create controller (endpoints) for login and sign-up users

--- dealit-restful-application/src/main/java/ir/dealit/restful/controller/auth/AuthenticationController.java
+++ dealit-restful-application/src/main/java/ir/dealit/restful/controller/auth/AuthenticationController.java
@@ -1,6 +1,13 @@
 package ir.dealit.restful.controller.auth;
 
+import ir.dealit.restful.dto.auth.AuthenticationRequest;
+import ir.dealit.restful.dto.auth.AuthenticationResponse;
+import ir.dealit.restful.dto.auth.RegisterRequest;
+import ir.dealit.restful.service.auth.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+    private final AuthenticationService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> registerUser(
+            @RequestBody RegisterRequest request
+    ) {
+        //Todo: implement register with AuthenticationService
+        return null;
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticateUser(
+            @RequestBody AuthenticationRequest request
+    ) {
+        //Todo: implement authenticate with AuthenticationService
+        return null;
+    }
 }
