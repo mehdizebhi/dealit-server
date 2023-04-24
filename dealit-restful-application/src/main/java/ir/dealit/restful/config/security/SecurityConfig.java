313a2c1beb3f926c37311ceabc06cb090dde01df
mehdizebhi <mehdizebhi@yahoo.com>
2023-04-24 11:42:00 +0330

    make primary bean of JwtUtil

--- dealit-restful-application/src/main/java/ir/dealit/restful/config/security/SecurityConfig.java
+++ dealit-restful-application/src/main/java/ir/dealit/restful/config/security/SecurityConfig.java
@@ -4,10 +4,12 @@ import ir.dealit.restful.config.security.jwt.util.JwtUtils;
 import ir.dealit.restful.config.security.jwt.util.JwtUtilsImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class SecurityConfig {
     @Bean
+    @Primary
     public JwtUtils getJwtUtilsBean() {
         return new JwtUtilsImpl();
     }
