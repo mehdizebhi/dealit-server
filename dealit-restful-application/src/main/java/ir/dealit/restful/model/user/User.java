f24b413e53983fc69f4c23b09538815e1884c467
mehdizebhi <mehdizebhi@yahoo.com>
2023-05-04 11:12:15 +0330

    Add createAt and updateAt in User model

--- dealit-restful-application/src/main/java/ir/dealit/restful/model/user/User.java
+++ dealit-restful-application/src/main/java/ir/dealit/restful/model/user/User.java
@@ -5,12 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 
 @Data
@@ -25,6 +28,8 @@ public class User implements UserDetails {
     private String password;
     private String displayName;
     private String email;
+    private @CreatedDate LocalDateTime createdAt;
+    private @LastModifiedDate LocalDateTime updatedAt;
     private boolean accountNonExpired;
     private boolean accountNonLocked;
     private boolean credentialsNonExpired;
