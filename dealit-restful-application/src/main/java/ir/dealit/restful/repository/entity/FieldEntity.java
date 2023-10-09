fc361c7706d026b50734a39d00bbef0e463ca0f3
mehdizebhi <mehdizebhi@yahoo.com>
2023-10-09 12:40:39 +0330

    Update Field Entity (add id property)

--- dealit-restful-application/src/main/java/ir/dealit/restful/repository/entity/FieldEntity.java
+++ dealit-restful-application/src/main/java/ir/dealit/restful/repository/entity/FieldEntity.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Document(collection = "fields")
 public class FieldEntity {
+    private @MongoId ObjectId id;
     private String title;
     private String description;
 }
