c1cbe7557822b8eaba57c87be087427a8b039fd9
mehdizebhi <mehdizebhi@yahoo.com>
2023-11-06 10:57:52 +0330

    Config Async Support for asynchronous method execution

---
+++ dealit-restful-application/src/main/java/ir/dealit/restful/config/app/SpringAsyncConfig.java
@@ -0,0 +1,9 @@
+package ir.dealit.restful.config.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class SpringAsyncConfig {
+}
