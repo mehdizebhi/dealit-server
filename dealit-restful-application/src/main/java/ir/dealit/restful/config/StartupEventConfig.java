65ef549f6b079ab2b821690a4e8e6203efbc2ae4
mehdizebhi <mehdizebhi@yahoo.com>
2023-11-06 10:56:33 +0330

    Add Start Up Event to execute some configuration whenever app is ready

---
+++ dealit-restful-application/src/main/java/ir/dealit/restful/config/StartupEventConfig.java
@@ -0,0 +1,25 @@
+package ir.dealit.restful.config;
+
+import ir.dealit.restful.config.db.MongoMigration;
+import ir.dealit.restful.util.event.StartupEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class StartupEventConfig {
+
+    private final MongoMigration mongoMigration;
+
+    @Async
+    @EventListener(StartupEvent.class)
+    public void handleStartupEvent(){
+        log.info("Application Starting up!");
+//        mongoMigration.init();
+        log.info("Application is ready now!");
+    }
+}
