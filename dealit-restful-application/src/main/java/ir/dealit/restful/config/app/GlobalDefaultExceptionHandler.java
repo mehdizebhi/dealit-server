6d40312f901e2ee7e29e40ace067c90f659ee237
mehdizebhi <mehdizebhi@yahoo.com>
2024-01-03 12:27:13 +0330

    Add log in GlobalDefaultExceptionHandler

--- dealit-restful-application/src/main/java/ir/dealit/restful/config/app/GlobalDefaultExceptionHandler.java
+++ dealit-restful-application/src/main/java/ir/dealit/restful/config/app/GlobalDefaultExceptionHandler.java
@@ -2,15 +2,18 @@ package ir.dealit.restful.config.app;
 
 import ir.dealit.restful.dto.common.ResponseModel;
 import ir.dealit.restful.util.exception.DealitException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalDefaultExceptionHandler {
 
     @ExceptionHandler(DealitException.class)
     protected ResponseEntity<ResponseModel<Void>> defaultErrorHandler(DealitException ex) {
+        log.error("[Global Default Exception Handler] - {}", ex.getMessage());
         return ResponseEntity.status(ex.getHttpStatus()).body((ResponseModel<Void>) ResponseModel.builder().error(ex).build());
     }
 }
