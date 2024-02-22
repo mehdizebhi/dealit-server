47db3f1212d148ef1236d1384653f27df2b050b0
mehdizebhi <mehdizebhi@yahoo.com>
2024-02-22 10:40:34 +0330

    update DealitRestfulApplication

--- dealit-restful-application/src/main/java/ir/dealit/restful/DealitRestfulApplication.java
+++ dealit-restful-application/src/main/java/ir/dealit/restful/DealitRestfulApplication.java
@@ -1,14 +1,7 @@
 package ir.dealit.restful;
 
-import ir.dealit.restful.service.SMSService;
-import ir.dealit.restful.util.event.StartupEvent;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DealitRestfulApplication {
