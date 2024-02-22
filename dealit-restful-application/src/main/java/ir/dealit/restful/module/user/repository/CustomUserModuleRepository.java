86241430c732650704d79e03281f99c7daac0bc3
mehdizebhi <mehdizebhi@yahoo.com>
2024-02-22 10:39:37 +0330

    update user repository

--- dealit-restful-application/src/main/java/ir/dealit/restful/module/user/repository/CustomUserModuleRepository.java
+++ dealit-restful-application/src/main/java/ir/dealit/restful/module/user/repository/CustomUserModuleRepository.java
@@ -1,8 +1,10 @@
 package ir.dealit.restful.module.user.repository;
 
 import ir.dealit.restful.module.user.entity.ConfirmationCodeEntity;
+import org.bson.types.ObjectId;
 
 public interface CustomUserModuleRepository {
 //    void updateConfirmationCode(ConfirmationCodeEntity confirmationCode);
 
+    void updateConnections(ObjectId userId, int newConnections);
 }
--- dealit-restful-application/src/main/java/ir/dealit/restful/module/user/repository/impl/CustomUserModuleRepositoryImpl.java
+++ dealit-restful-application/src/main/java/ir/dealit/restful/module/user/repository/impl/CustomUserModuleRepositoryImpl.java
@@ -1,8 +1,13 @@
 package ir.dealit.restful.module.user.repository.impl;
 
+import ir.dealit.restful.module.user.entity.UserEntity;
 import ir.dealit.restful.module.user.repository.CustomUserModuleRepository;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,4 +16,11 @@ public class CustomUserModuleRepositoryImpl implements CustomUserModuleRepositor
 
     private final MongoTemplate mongoTemplate;
 
+    @Override
+    public void updateConnections(ObjectId userId, int newConnections) {
+        Query query = new Query(Criteria.where("_id").is(userId));
+        Update update = new Update().set("connections", newConnections);
+
+        mongoTemplate.updateFirst(query, update, UserEntity.class);
+    }
 }
