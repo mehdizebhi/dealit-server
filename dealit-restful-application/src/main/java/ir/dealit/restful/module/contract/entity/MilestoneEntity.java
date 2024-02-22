46f1fd1d880230223680d64f8836db06c0fd53bc
mehdizebhi <mehdizebhi@yahoo.com>
2024-02-22 10:41:06 +0330

    update contract entity

--- dealit-restful-application/src/main/java/ir/dealit/restful/module/contract/entity/MilestoneEntity.java
+++ dealit-restful-application/src/main/java/ir/dealit/restful/module/contract/entity/MilestoneEntity.java
@@ -5,9 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 @Data
 @Builder
 @NoArgsConstructor
-//@AllArgsConstructor
+@AllArgsConstructor
 public class MilestoneEntity {
+
+    private int step;
+    private BigDecimal priceRate;
+    private String description;
+    private Date startedAt;
+    private Date endedAt;
 }
